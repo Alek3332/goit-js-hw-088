ac50c17eb5b406c6c4820c44bf712bacd0fc409a
Alek3332 <118377866+Alek3332@users.noreply.github.com>
2023-07-28 16:47:25 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,6 @@
 import { galleryItems } from './gallery-items';
-
 import SimpleLightbox from 'simplelightbox';
-
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Change code below this line
 
 console.log(SimpleLightbox);
 
